0974761bea764e4beb4ae717dd87e4c075a5bc05
williamsolon <77649859+williamsolon@users.noreply.github.com>
2022-03-30 19:00:55 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" type="image-x/png" href="images/logo_nave_espacial_ico.ico">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="css/style.css" />
+
+      <title>Nave Espacial | Estação Amorzinho</title>
+    </head>
+
+    <body>
+
+      <header class="header1">
+        <?php require_once "pages/header3.php" ?>
+      </header>
+
+      <section class="title">
+        
+          <div class="container">
+            <a href="index.php">
+              <img src="images/logo_nave_espacial_png.png">
+              <h2>Nave Espacial</h2>
+              <span>Estação Amorzinho</span>
+            </a>
+          </div>
+        
+      </section>
+
+      <section class="cards">
+        <div class="container">
+
+          <a href="pages/sala.php" class="cards_single">
+            <div style="background-image: url('images/sala/sala1.jpg');" class="cards_single_img"></div>
+            <h2>Sala</h2>
+          </a>
+
+          <a href="pages/cozinha.php" class="cards_single">
+            <div style="background-image: url('images/cozinha/cozinha1.jpg');" class="cards_single_img"></div>
+            <h2>Cozinha</h2>
+          </a>
+
+          <a href="pages/banheiro.php" class="cards_single">
+            <div style="background-image: url('images/banheiro/banheiro1.jpg');" class="cards_single_img"></div>
+            <h2>Banheiro</h2>
+          </a>
+
+          <a href="pages/quarto.php" class="cards_single">
+            <div style="background-image: url('images/quarto/quarto1.jpg');" class="cards_single_img"></div>
+            <h2>Quarto</h2>
+          </a>
+
+          <a href="pages/atelie.php" class="cards_single">
+            <div style="background-image: url('images/atelie/atelie1.jpg');" class="cards_single_img"></div>
+            <h2>Ateliê</h2>
+          </a>
+
+          <a href="pages/estudio.php" class="cards_single">
+            <div style="background-image: url('images/estudio/estudio1.jpg');" class="cards_single_img"></div>
+            <h2>Estúdio/Escritório</h2>
+          </a>
+
+          <a href="pages/natal.php" class="cards_single">
+            <div style="background-image: url('images/natal/natal1.jpg');" class="cards_single_img"></div>
+            <h2>Natal</h2>
+          </a>
+
+          <a href="pages/outros.php" class="cards_single">
+            <div style="background-image: url('images/outros/outros1.jpg');" class="cards_single_img"></div>
+            <h2>Outros</h2>
+          </a>
+
+        </div>
+      </section>
+
+      <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+      <script src="https://use.fontawesome.com/1744f3f671.js"></script>
+      <script src="js/script.js"></script>
+
+    </body>
+  </html>
